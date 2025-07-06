60abbeb7a3d58a3c08b59191722db03041057675
Wendell Lance <wendellravago@gmail.com>
2025-07-06 17:25:17 +0800

    update compare password logic for salt

--- src/utils/scrypt.ts
+++ src/utils/scrypt.ts
@@ -1,15 +1,20 @@
 import crypto from 'crypto'
 
-export const hashPassword = (password: string): string => {
+export const hashPassword = (password: string, givenSalt?: string): string => {
     const salt = crypto.randomBytes(16).toString("hex")
 
-    const result = crypto.scryptSync(password, salt, 64).toString('hex')
+    const result = crypto.scryptSync(password, givenSalt ?? salt, 64).toString('hex')
 
     return `${result}.${salt}`
 }
 
 export const comparePassword = (given: string, password: string): boolean => {
-    const passwordBuf = Buffer.from(password.split('.')[0], 'hex')
-    const givenBuf = Buffer.from(hashPassword(given), 'hex')
+
+    const [hash, salt] = password.split('.')
+
+    const passwordBuf = Buffer.from(hash, 'hex')
+    const givenBuf = Buffer.from(hashPassword(given, salt), 'hex')
+
+    console.log(passwordBuf.toString('hex'), givenBuf.toString('hex'))
     return crypto.timingSafeEqual(passwordBuf, givenBuf)
 }
